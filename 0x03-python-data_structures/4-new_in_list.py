2015db46ef84f9e0d721639fdad45bbb44056e57
Polarthebear <thatotae325@gmail.com>
2024-01-23 06:06:56 -0800

    Function that replaces an element in a list at a specific without modifying the original list.

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,8 @@
 def new_in_list(my_list, idx, element):
 
     clone = my_list.copy()
-    if idx < 0 or idx > len(my_list):
-        return my_list
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.copy()
     else:
         clone[idx] = element
     return clone
