36d42a290faf452a5134b3128d8825fcdbec16e4
Polarthebear <thatotae325@gmail.com>
2024-01-24 10:48:56 -0800

    A square that defines a square based on 0-sqaure.py).

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+"""defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+
+    """Representing a square."""
+
+    def __init__(self, size):
+        """Initializes the new square.
+
+        Args:
+            size = size of initialized square.
+        """
+        self.__size = size
