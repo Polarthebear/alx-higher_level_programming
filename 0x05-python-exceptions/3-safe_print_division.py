6a72698e5f772127d9f81b2fc9d3e5cbad0e20c8
Polarthebear <thatotae325@gmail.com>
2024-02-09 07:33:00 -0800

    Function that divides 2 ints and prints the result.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    """Return the duvision of ints a and b"""
     try:
         div = a / b
     except (TypeError, ZeroDivisionError):
         div = None
     finally:
-        print("Inside Result: {}".format(div))
+        print("Inside result: {}".format(div))
     return (div)
