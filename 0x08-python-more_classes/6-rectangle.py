e63cddeac18a7e736b6050d2cca9be5e134a5e37
Polarthebear <thatotae325@gmail.com>
2024-02-14 11:45:35 -0800

    A class that defines a rectangle based on 5-rectangle.

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """this represents a rectangle"""
-    numbe_of_instances = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initializing this rectangle class
