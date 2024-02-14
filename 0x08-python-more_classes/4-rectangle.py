c512ad18ec5fbff7b1677dabccce53f47e4fa987
Polarthebear <thatotae325@gmail.com>
2024-02-14 10:53:01 -0800

    A class that defines a rectangle based on 3-rectangle.

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Defines a rectangle by: (based on 3-rectangle.py)"""
+
+
+class Rectangle:
+    """Representing a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """ Initializing a new rectangle.
+
+        Args:
+        width (int): width of the rectangle
+        height (int): height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setting width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setting height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return the area of the particular rectangle."""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Return the perimeter of the particular rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        else:
+            return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Printing a rectangle using the # character."""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        recta = []
+        for n in range(self.__height):
+            [recta.append('#') for m in range(self.__width)]
+            if n != self.__height - 1:
+                recta.append('\n')
+        return ("".join(recta))
+
+    def __repr__(self):
+        """Return string representation of the rectangle."""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
