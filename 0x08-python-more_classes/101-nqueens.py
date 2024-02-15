4ddcb8f68ef1cae88935667553444c13e7b4410d
Polarthebear <thatotae325@gmail.com>
2024-02-14 23:22:51 -0800

    Advanced Task.

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,66 +1,57 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle"""
-
 import sys
 
-def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
-    return [[' ' for _ in range(n)] for _ in range(n)]
+def is_safe(board, row, col, n):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 'Q':
+            return False
 
-def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
-    return [[r, c] for r in range(len(board)) for c in range(len(board)) if board[r][c] == "Q"]
-
-def mark_attacked(board, row, col):
-    """Mark attacked positions on a chessboard."""
-    n = len(board)
-    for i in range(n):
-        board[row][i] = "x"  # Mark horizontally
-        board[i][col] = "x"  # Mark vertically
-
-    # Mark diagonals
+    # Check upper left diagonal
     for i, j in zip(range(row-1, -1, -1), range(col-1, -1, -1)):
-        if i >= 0 and j >= 0:
-            board[i][j] = "x"  # Mark diagonally up-left
-    for i, j in zip(range(row-1, -1, -1), range(col+1, n)):
-        if i >= 0 and j < n:
-            board[i][j] = "x"  # Mark diagonally up-right
-    for i, j in zip(range(row+1, n), range(col-1, -1, -1)):
-        if i < n and j >= 0:
-            board[i][j] = "x"  # Mark diagonally down-left
-    for i, j in zip(range(row+1, n), range(col+1, n)):
-        if i < n and j < n:
-            board[i][j] = "x"  # Mark diagonally down-right
+        if board[i][j] == 'Q':
+            return False
 
-def recursive_solve(board, row, queens, solutions):
-    """Recursively solve an N-queens puzzle."""
-    n = len(board)
-    if queens == n:
-        solutions.append(get_solution(board))
-        return solutions
+    # Check upper right diagonal
+    for i, j in zip(range(row-1, -1, -1), range(col+1, n)):
+        if board[i][j] == 'Q':
+            return False
+
+    return True
+
+def solve_nqueens(board, row, n, solutions):
+    if row == n:
+        solutions.append([''.join(row) for row in board])
+        return
 
     for col in range(n):
-        if board[row][col] == ' ':
-            tmp_board = [row.copy() for row in board]
-            tmp_board[row][col] = 'Q'
-            mark_attacked(tmp_board, row, col)
-            solutions = recursive_solve(tmp_board, row + 1, queens + 1, solutions)
+        if is_safe(board, row, col, n):
+            board[row][col] = 'Q'
+            solve_nqueens(board, row + 1, n, solutions)
+            board[row][col] = ' '
 
-    return solutions
+def print_solutions(n):
+    board = [[' ' for _ in range(n)] for _ in range(n)]
+    solutions = []
+    solve_nqueens(board, 0, n, solutions)
+
+    for solution in solutions:
+        for row in solution:
+            print(row)
+        print()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if not sys.argv[1].isdigit():
+
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            raise ValueError("N must be at least 4")
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
 
-    board = init_board(int(sys.argv[1]))
-    solutions = recursive_solve(board, 0, 0, [])
-    for sol in solutions:
-        print(sol)
+    print_solutions(n)
 
