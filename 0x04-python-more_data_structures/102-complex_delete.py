d7427abf00a8648beed0237e487c6321dff3ff2d
Polarthebear <thatotae325@gmail.com>
2024-02-15 12:10:53 -0800

    Advanced task.

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    list_key =list(a_dictionary.keys())
-
+    list_key = list(a_dictionary.keys())
 
     for value_del in list_key:
         if value == a_dictionary.get(value_del):
             del a_dictionary[value_del]
-
 
     return (a_dictionary)
