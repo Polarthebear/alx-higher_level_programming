8d74eb6e50515b87ed68759e77010d9d21df87b9
Polarthebear <thatotae325@gmail.com>
2024-02-28 07:18:12 -0800

    class BaseGeometry(based on 6-base_geometry.py)

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,5 +22,5 @@ class BaseGeometry:
         """
         if type(value) is not int:
             raise TypeError("<name> must be an integer")
-        if type(value) <= 0:
+        if value <= 0:
             raise ValueError("<name> must be greater than 0")
