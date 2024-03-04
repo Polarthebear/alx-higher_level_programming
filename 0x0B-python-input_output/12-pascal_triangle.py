662095280aa148ff0472b8c19e06cfd55b0b730d
Polarthebear <thatotae325@gmail.com>
2024-03-04 01:08:54 -0800

    Function that returns a list of lists of integers representing the Pascals Triangle of n.

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Function that defines a function of Pascal's Triangle."""
+
+
+def pascal_triangle(n):
+    """representing Pascal's Triangle of size (n).
+
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    tri = [[1]]
+    while len(tri) != n:
+        triangles = tri[-1]
+        tmp = [1]
+        for i in range(len(triangles) - 1):
+            tmp.append(triangles[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
