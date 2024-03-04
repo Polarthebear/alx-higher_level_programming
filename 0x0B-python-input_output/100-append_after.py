9ab209f5ab947d33d998995a87aa85b536885563
Polarthebear <thatotae325@gmail.com>
2024-03-04 01:40:22 -0800

    Advanced Task."

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Function to insert text after each line containing goiven string.
+
+    Args:
+        filename: (str) Name of file.
+        search_string: (str) string to search for within file.
+        new_string: (str) string to insert.
+    """
+    txt = ""
+    with open(filename) as r:
+        for line in r:
+            txt += line
+            if search-string in line:
+                txt += new_string
+    with open(filename, "w") as w:
+        w.write(text)
