9c38945f07ad9114b5e2deb6a8a0144f8ea1e70b
polarthebear <thatotae325@gmail.com>
2024-03-11 21:02:18 +0000

    Advanced Task.

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,29 @@
+#include "Python.h"
+
+/**
+ * print_python_string - Main entry point
+ * Description: Prints information about python strings
+ * @p: PyObj string object
+ */
+void print_python_string(PyObject *p)
+{
+	long int leng;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	leng = ((PyASCIIObject *)(p))->leng;
+
+	if (PyUniCode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf(" length: %ld\n", leng);
+	printf(" value: %ls\n", PyUniCode_AsWideCharString(p, &lenh));
+}
