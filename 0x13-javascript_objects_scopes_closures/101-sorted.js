f3aae0c697934c84489c04a465dd278c3b6a400c
Polarthebear <thatotae325@gmail.com>
2024-05-01 13:14:05 -0700

    Advanced Task.

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const dict = require('./101-data.js').dict;
-let listed = {};
-for (let key in dict) {
+const listed = {};
+for (const key in dict) {
   if (listed[dict[key]] === undefined) {
     listed[dict[key]] = [key];
   } else {
